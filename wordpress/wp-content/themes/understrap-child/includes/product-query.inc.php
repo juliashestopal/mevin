4987f8b2181d451562a6d6e0782c567ed1b7319a
germansmolyar <german@crunchmind.com>
2020-05-05 21:39:18 +0300

    Change Typo for impact drivers

--- wordpress/wp-content/themes/understrap-child/includes/product-query.inc.php
+++ wordpress/wp-content/themes/understrap-child/includes/product-query.inc.php
@@ -75,8 +75,8 @@ function calculate_relevance($post, $queryArray)
                 }
             }
         }
-        if (get_post_meta($post->ID, 'post_views_count', true) > 0 && get_post_meta($post->ID, 'clicks', true) > 0) {
-            $raw_score = $raw_score + (int)get_post_meta($post->ID, 'clicks', true) / (int)get_post_meta($post->ID, 'post_views_count', true);
+        if (get_post_meta($post->ID, 'views', true) > 0 && get_post_meta($post->ID, 'clicks', true) > 0) {
+            $raw_score = $raw_score + (int)get_post_meta($post->ID, 'clicks', true) / (int)get_post_meta($post->ID, 'views', true);
         }
     }
     //echo $post->post_title .': '.($raw_score) / $condition_count .'<br>';
